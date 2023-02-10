e035163a52e8d48a47740bb766005087e695ba56
Shahan <shahanmac@Shahans-MacBook-Pro.local>
2023-02-10 17:23:20 +0600

    updated

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+$('.loader_bg').delay(1000).fadeOut('slow');
+$('.loaderlogoDiv').delay(1000).fadeOut('slow');
+
+$(document).ready(function () {
+    $('.body').hide().delay(1000).fadeIn('slow');
+});
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('loaderOwl'),
+    path: './Assets/loader.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('logo'),
+    path: './Assets/logo.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('character'),
+    path: './Assets/character.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('hello'),
+    path: './Assets/hello.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+});
+
+
